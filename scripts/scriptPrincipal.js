3753ddcd79342653312870026454613e853b957c
Braian-de-Liz <braian_l_silva@estudante.sesisenai.org.br>
2025-08-01 23:19:23 -0300

    adições

--- scripts/scriptPrincipal.js
+++ scripts/scriptPrincipal.js
@@ -1,3 +1,4 @@
+const MostrarRes = document.getElementById("Mostrar");
 const LimpaResult = document.getElementById("C");
 const LimparNum = document.getElementById("backstao");
 
@@ -32,6 +33,11 @@ function apagar() {
     document.getElementById("Mostrar").innerHTML = apagarElemento.substring(0, apagarElemento.length - 1)
 }
 
+function calcularVarios(NumEscrito) {
+    NumEscrito = document.getElementById('Mostrar').innerHTML.eval();
+}
+
+const igual = document.getElementById("=");
 
 LimpaResult.addEventListener("click", limpar);
 
@@ -44,3 +50,18 @@ LimparNum.addEventListener("keydown", (event) => {
 LimparNum.addEventListener("click", apagar);
 
 
+igual.addEventListener("click",)
+
+if (MostrarRes.value.length > 8) {
+    
+}
+
+
+/* function soma2() {
+    let num1 = +prompt("digiga o numero 1º");
+    let num2 = +prompt("digiga o numero 2º");
+
+    const resultadooo = num1 + num2;
+
+    alert(resultadooo);
+} */
